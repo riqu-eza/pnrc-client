332d577240ed7af14fa5abf4d4cfc28b3211399e
kang`ethe <offline@kali.dkmuthunga.com>
2025-09-25 14:50:02 +0300

    on

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://pnrc.onrender.com/',
-        secure: false,
+      "/api": {
+        target: "https://pnrc.onrender.com/",
+        changeOrigin: true, // <-- important to avoid CORS issues
+        secure: true,
       },
     },
   },
   plugins: [react()],
-})
+});
